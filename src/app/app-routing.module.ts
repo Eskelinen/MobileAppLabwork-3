077dcb2efb06b464f61d7e36acaef00b75effd32
Eskelinen <toivo.eskelinen@gmail.com>
2023-03-22 20:23:21 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,29 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'loader',
     pathMatch: 'full'
   },
+
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'loader',
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+  },
+  {
+    path: 'homepage',
+    loadChildren: () => import('./pages/homepage/homepage.module').then( m => m.HomepagePageModule)
+  },
+  {
+    path: 'second',
+    loadChildren: () => import('./pages/second/second.module').then( m => m.SecondPageModule)
+  },
+  {
+    path: 'third',
+    loadChildren: () => import('./pages/third/third.module').then( m => m.ThirdPageModule)
+  },
+  {
+    path: 'fourth',
+    loadChildren: () => import('./pages/fourth/fourth.module').then( m => m.FourthPageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+    { title: "Home", url: "/homepage", icon: "mail"},
+    { title: "Chimpanzee", url: "/second", icon: "mail"},
+    { title: "Orangutan", url: "/third", icon: "mail"},
+    { title: "Baboon", url: "/fourth", icon: "mail"}
+ ];
+ constructor() {}
 }
